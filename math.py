5331d3e2594194ea2f428dd28720bd02b510d415
kalyanpenmetsa <penmetsa29@gmail.com>
2018-06-09 21:03:09 +0000

    Added fibonacci series function

---
+++ math.py
@@ -0,0 +1,13 @@
+#Triangular series addition
+def tri(n):
+        n = n + 1
+        sum=0
+        for i in range(1,n+1):
+                sum = sum + (n-i)
+        print(sum)
+#Fibonacci Function
+def fibo(n):
+        fibolist = [1,2]
+        for i in range(1,n-1):
+                fibolist.append(fibolist[i] + fibolist[i-1])
+        print(fibolist)
