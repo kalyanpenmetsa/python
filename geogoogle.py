578093aeb6b8e60110e9f9063620b1b1300d55e8
kalyanpenmetsa <penmetsa29@gmail.com>
2018-06-14 03:52:32 -0500

    Updated with db insert capability

--- geogoogle.py
+++ geogoogle.py
@@ -1,7 +1,12 @@
 import urllib
 import json
-
+import sqlite3
 serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+
+conn = sqlite3.connect('geodb.sqlite')
+cursor = conn.cursor()
+cursor.execute('DROP TABLE IF EXISTS Geodata')
+cursor.execute('CREATE TABLE Geodata(place VARCHAR(128), Address VARCHAR(128), Lat VARCHAR(128), Long VARCHAR(128), County VARCHAR(128), City VARCHAR(128))')
 
 while True:
     address = raw_input('Enter location: ')
@@ -25,10 +30,15 @@ while True:
         print(data)
         continue
 
-    print(json.dumps(js, indent=4))
+#    print(json.dumps(js, indent=4))
 
     lat = js["results"][0]["geometry"]["location"]["lat"]
     lng = js["results"][0]["geometry"]["location"]["lng"]
-    print('lat', lat, 'lng', lng)
+    cou = js["results"][0]["address_components"][4]["long_name"]
     location = js['results'][0]['formatted_address']
+    city = js["results"][0]["address_components"][5]["long_name"]
+    print('lat', lat, 'lng', lng, 'county', cou, 'address', location, 'city', city)
+    cursor.execute('insert into Geodata(place,Address,Lat,Long,County,City) values (?,?,?,?,?,?)', (address,location,lat,lng,cou,city,))
     print(location)
+
+conn.commit()
