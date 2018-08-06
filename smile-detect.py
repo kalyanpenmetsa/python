3afe738f799d480645450b9970f7f476112f2f2e
kalyanpenmetsa <penmetsa29@gmail.com>
2018-08-05 23:33:53 -0500

    Update smile-detect.py

--- smile-detect.py
+++ smile-detect.py
@@ -1,9 +1,6 @@
-####Download numpy and opencv-python modules using pip install
 import cv2
 import numpy as np
 import sys
-
-####Download face and smile filters from https://github.com/opencv/opencv/tree/master/data/haarcascades/ ####
 faceFilter = 'data/haarcascades/haarcascade_frontalface_default.xml'
 smileFilter = 'data/haarcascades/haarcascade_smile.xml'
 
@@ -11,22 +8,27 @@ faceCascade = cv2.CascadeClassifier(faceFilter)
 smileCascade = cv2.CascadeClassifier(smileFilter)
 
 cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
+cap.set(3, 640)  # set Width
+cap.set(4, 480)  # set Height
 
 while True:
     ret, frame = cap.read()
-#    vid = cv2.cvtColor(frame, 0)
-    vid = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # vid = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    vid = cv2.cvtColor(frame, 0)
+    faces = faceCascade.detectMultiScale(vid, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
 
-    faces = faceCascade.detectMultiScale(vid, scaleFactor=1.05, minNeighbors=8, minSize=(55, 55))
-    cv2.imshow('frame', vid)
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        smile = smileCascade.detectMultiScale(vid, scaleFactor=1.7, minNeighbors=22, minSize=(25, 25))
+        cv2.rectangle(vid, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = vid[y:y + h, x:x + w]
+        roi_color = vid[y:y + h, x:x + w]
+
+        smile = smileCascade.detectMultiScale(frame, scaleFactor=1.7, minNeighbors=22, minSize=(25, 25))
         # print("Found", len(smile), "smiles!")
         for (x, y, w, h) in smile:
             print("Found", len(smile), "smiles!")
+            cv2.rectangle(vid, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow('frame', vid)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
