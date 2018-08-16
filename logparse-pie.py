b140ac9ee35e997e06cfa5cb750677e0d6bb806e
kalyanpenmetsa <penmetsa29@gmail.com>
2018-08-15 21:36:53 -0500

    Update logparse-pie.py

--- logparse-pie.py
+++ logparse-pie.py
@@ -1,26 +1,47 @@
+import matplotlib
+import numpy as np
 import matplotlib.pyplot as plt
-f = open('/var/log/yum.log')
-I = {}
-c = 0
-d = 0
-e = 0
-for i in f.readlines():
-    # print(i.split(' ')[3])
-    if 'Installed' in i.split(' ')[3]:
-        I['Installed'] = c
-        c = c + 1
-    elif 'Updated' in i.split(' ')[3]:
-        I['Updated'] = d
-        d = d + 1
-    elif 'Erased' in i.split(' ')[3]:
-        I['Erased'] = e
-        e = e + 1
-labels = 'Installed','Updated','Erased'
-sizes = [I['Installed'],I['Updated'],I['Erased']]
-print(sizes)
-fig1, ax1 = plt.subplots()
-# ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-ax1.pie(sizes, labels=labels, explode=None, autopct='%1.1f%%', shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-plt.show()
+class drawPie:
+    def openFile(self,logPath):
+        self.logPath = logPath
+        f = open(logPath)
+        return f.read()
+
+    def drawPie(self,explode,labelList,sizeList,autoPct='%1.1f%%',shadow=True,startAngle='90'):
+        self.explode = explode
+        self.labelList = labelList
+        self.sizeList = sizeList
+        self.autoPct = autoPct
+        self.shadow = shadow
+        self.startAngle = startAngle
+
+        if len(labelList) == len(sizeList):
+            fig1, ax1 = plt.subplots()
+            ax1.pie(sizeList, explode=explode, labels=labelList, autopct=autoPct, shadow=shadow, startangle=90)
+            ax1.axis('equal')
+            return plt.show()
+        else:
+            print("len(labelList) not equal to len(sizeList)")
+            print(pie.openFile('error.log'))
+
+#pie.drawPie(logPath,explodeList,labelList,sizeList)
+pie.drawPie([0,0,0,0],['Info','Access','Warning','Error'],[1,2,3,4])
+
+# print(np.arange(3))
+
+
+class drawBar:
+    def drawBar(self,xList,yList):
+        self.xList = xList
+        self.yList = yList
+        if len(xList) == len(yList):
+            plt.bar(xList,yList)
+            plt.show()
+        else:
+            print("len(xList) not equal to len(yList)")
+
+pie = drawPie()
+bar = drawBar()
+bar.drawBar([0,1,2],[1,2,4])
+print(bar.yList)
